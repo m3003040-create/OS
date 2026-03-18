3cfd1f8bc89df9320c22bcbc876d5a55e587071c
m3003040-create <m3003040@gmail.com>
2026-03-18 20:27:42 +0300

    Create io.h

---
+++ io.h
@@ -0,0 +1,14 @@
+#ifndef IO_H
+#define IO_H
+
+static inline void outb(unsigned short port, unsigned char val) {
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+static inline unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    asm volatile ( "inb %1, %0" : "=a"(ret) : "Nd"(port) );
+    return ret;
+}
+
+#endif
